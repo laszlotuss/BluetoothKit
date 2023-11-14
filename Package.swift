675c7fee7f534920d6c31c1a44bc67fd08aa7d61
László Tuss <2952613+laszlotuss@users.noreply.github.com>
2023-11-14 16:29:00 +0100

    Added watchOS 4.0 to SPM platforms

--- Package.swift
+++ Package.swift
@@ -8,7 +8,8 @@ let package = Package(
     platforms: [
         .iOS(.v10),
         .macOS(.v10_10),
-        .tvOS(.v9)
+        .tvOS(.v9),
+        .watchOS(.v4),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
